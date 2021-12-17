f34c3a9c1703acc0eb1b25b7aa0bcaed0e7edba1
vipulsingh7731 <vipulsingh7731@gmail.com>
2021-12-17 13:32:20 +0530

    Updated pdf_downloader for pikepdf

--- pdf_downloader/pdf_download_final.py
+++ pdf_downloader/pdf_download_final.py
@@ -3,21 +3,10 @@ from bs4 import BeautifulSoup
 # to use webdriver you need chromedriver.exe in the same folder as the .py script
 from selenium import webdriver
 import time
-from PyPDF2 import PdfFileMerger
 import os
 import csv
 import random
-
-
-def get_html_source(url, sleep_time=10):
-    browser =  webdriver.Edge(r"msedgedriver.exe")
-    # get web page
-    browser.get(url)
-    # execute script to scroll down the page
-    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-    # sleep for time
-    time.sleep(sleep_time)
-    return browser.page_source
+from pikepdf import Pdf as PDF
 
 # returns list of links of all pdf on a page
 def get_all_hrefs(list_of_page_number_on_website, sleep_time=10):
@@ -47,32 +36,47 @@ def get_all_hrefs(list_of_page_number_on_website, sleep_time=10):
 
 # Create and merge pdfs
 def create_and_merge_pdfs(links_list, start_page, end_page):
-    pdf_name_list = []
+    pdf_name_list_which_were_created = []
     initial_start_page = start_page
     for a_page in links_list:
-        merger = PdfFileMerger()
+        pdf_ = PDF.new()
+        version = pdf_.pdf_version
+        if len(links_list[start_page-initial_start_page]) == 0:
+            print(f"---------Index {start_page} has no files")
+            pdf_.close()
+            start_page += 1
+            continue
         for index_link, a_link in enumerate(a_page):
             content = requests.get(a_link).content
             with open(f"pdf_downloader_folder/{start_page}_{index_link}.pdf", 'wb') as my_data:
                 my_data.write(content)
-            pdf_name_list.append(f"pdf_downloader_folder/{start_page}_{index_link}.pdf")
+                my_data.close()
+            pdf_name_list_which_were_created.append(f"pdf_downloader_folder/{start_page}_{index_link}.pdf")
             
             try:
-                merger.append(f"pdf_downloader_folder/{start_page}_{index_link}.pdf")
+                
+                src = PDF.open(f"pdf_downloader_folder/{start_page}_{index_link}.pdf")
+                version = max(version, src.pdf_version)
+                pdf_.pages.extend(src.pages)
+                src.close()
                 print(f"Fetched & Merged PageNumber_PDFindex-- {start_page}_{index_link}")
             except:
                 print(f"------------{start_page}_{index_link}.pdf was not merged due to some reason!!!")
-                print("Visit the folling link for it:")
-                print(f"Link--- {a_link}")
+                print("----It is written to a separate file")
+                with open(f"pdf_downloader_folder/Compiled_{start_page}_{index_link}_was.pdf", 'wb') as f:
+                    f.write(content)
+                    f.close()
             time.sleep(random.random()*3)
-
-        merger.write(f"pdf_downloader_folder/Compiled_{start_page}_.pdf")
-        merger.close()
+        pdf_.remove_unreferenced_resources()
+        pdf_.save(f"pdf_downloader_folder/Compiled_{start_page}_.pdf", min_version=version)
+        pdf_.close()
+        # merger.write(f"pdf_downloader_folder/Compiled_{start_page}_.pdf")
+        # merger.close()
     
         start_page += 1
 
-    my_data.close()
-    for pdf in pdf_name_list:
+    
+    for pdf in pdf_name_list_which_were_created:
         os.remove(pdf)
     print("Compiled PDF Created")
     return
@@ -82,6 +86,10 @@ def create_and_merge_pdfs(links_list, start_page, end_page):
 print("-- Enter the Page number of the website ,i.e, what you write after / . P.S. Both are included")
 start_page= int(input("Start_page - "))
 end_page= int(input("End_page - "))
+if start_page>end_page:
+    new_var = start_page
+    start_page = end_page
+    end_page = new_var
 mylist = list(range(start_page, end_page+1))
 print("-- Whether to read from previously saved CSV or not (saved inside pdf_downloader_folder) ----")
 print("-- Use 'w' if you have no idea about this ----")
